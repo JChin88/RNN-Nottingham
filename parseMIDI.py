f734172392f48dcc3a8547f9cade1f2057c8e9e6
root <root@LAPTOP-NLT9RP2Q.localdomain>
2018-10-18 16:51:12 -0400

    implemented note sequencer

--- parseMIDI.py
+++ parseMIDI.py
@@ -1,9 +1,40 @@
 from mido import MidiFile
+import torch
 
-filename = 'NottinghamDataset/nottingham-dataset-master/MIDI/ashover1.mid'
+# Parse a MIDI file by finding its notes and lengths
+# Note Representation: [note, velocity, length, time]
+def parseNotes(filename):
+	sequence = []
+	mid = MidiFile(filename)
+	track = mid.tracks[0]
+	elapsedTime = 0
+	for i, msg in enumerate(track):
+		elapsedTime += msg.time
+		if msg.type == 'note_on':
+			j = i + 1
+			length = 0
+			noteOff = False
+			while not noteOff and j < len(track):
+				nxtmsg = track[j]
+				length += nxtmsg.time
+				if nxtmsg.type == 'note_off' and nxtmsg.note == msg.note:
+					sequence += [noteToTensor([msg.note, msg.velocity, length, elapsedTime])]
+					noteOff = True
+				j += 1
+	return sequence
+
+# Parse a MIDI file by reading it by time(beats)
+def parseBeats(filename):
+	#TODO: implement parsing of beats
+	# Look for Time Signature Message
+	return
+
+def noteToTensor(note):
+	return torch.tensor(note)
+
+filename = 'MIDI/ashover1.mid'
 mid = MidiFile(filename)
 
-for i, track in enumerate(mid.tracks):
-	print('Track {}: {}'.format(i, track.name))
-	for msg in track:
-		print(msg)
+sequence = parseNotes(filename)
+for note in sequence:
+	print(note)
