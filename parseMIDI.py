bb7cfb891e5b8a7f9be7f422c7d821f8d45aec8c
root <root@LAPTOP-NLT9RP2Q.localdomain>
2018-10-25 18:08:30 -0400

    implemented RNN and parseBeats

--- parseMIDI.py
+++ parseMIDI.py
@@ -1,5 +1,8 @@
 from mido import MidiFile, MidiTrack, Message
-import torch
+
+#return the ticks per beat
+def getTpb(filename):
+	return MidiFile(filename).ticks_per_beat
 
 # Parse a MIDI file by finding its notes and lengths
 # Note Representation: [note, velocity, length, time]
@@ -21,15 +24,52 @@ def parseNotes(filename, tracknumber = 0):
 					sequence += [[msg.note, msg.velocity, length, elapsedTime]]
 					noteOff = True
 				j += 1
-	return torch.tensor(sequence)
+	return torch.tensor(sequence).float()
 
 # Parse a MIDI file by reading it by time(beats)
-def parseBeats(filename):
+def parseBeats(filename, tracknumber = 0, subdivisions = 2):
 	#TODO: implement parsing of beats
-	# Look for Time Signature Message
-	return
+	# Look for ticks per beat
+	sequence = []
+	mid = MidiFile(filename)
+	track = mid.tracks[tracknumber]
+	tpb = mid.ticks_per_beat
+	divisions = int(tpb / subdivisions)
+	curTone = 0
+	curVel = 0
+	numticks = 0
+	for msg in track:
+		numticks = 0
+		while numticks < msg.time:
+			sequence += [noteToBin(curTone, curVel)]
+			numticks += divisions
+		if msg.type == 'note_on':
+			curTone = msg.note
+			curVel = msg.velocity
+		elif msg.type == 'note_off':
+			curTone = 0
+			curVel = 0
+	return sequence
 
-#reconstructs a MIDI file from a list of note tensors
+#converts a note ([tone, velocity] pair) into a binary representation
+def noteToBin(tone, velocity):
+	btone = '{0:07b}'.format(tone)
+	bvel = '{0:07b}'.format(velocity)
+	note = []
+	for b in btone + bvel:
+		note += [int(b)]
+	return note
+
+#converts a binary value list of length 16 into a note
+def binToNote(binaryList):
+	# print(binaryList)
+	if len(binaryList) != 14:
+		raise ValueError('list must have length 14')
+	note = ''.join(str(n) for n in binaryList[0:7])
+	velocity = ''.join(str(v) for v in binaryList[7:])
+	return (int(note, 2), int(velocity, 2))
+
+#reconstructs a MIDI file from a list of notes
 def reconstructFromNotes(filename, sequence, ticks_per_beat):
 	mid = MidiFile()
 	track = MidiTrack()
@@ -40,10 +80,10 @@ def reconstructFromNotes(filename, sequence, ticks_per_beat):
 	noteOffs = []
 
 	for note in sequence:
-		tone = note[0].item()
-		velocity = note[1].item()
-		length = note[2].item()
-		time = note[3].item() - elapsedTime
+		tone = int(note[0].item())
+		velocity = int(note[1].item())
+		length = int(note[2].item())
+		time = int(note[3].item()) - elapsedTime
 
 		for noteOff in noteOffs:
 			if noteOff[1] <= time:
@@ -59,36 +99,52 @@ def reconstructFromNotes(filename, sequence, ticks_per_beat):
 	mid.save('output/' + filename)
 	return mid
 
-filename = 'MIDI/melody/ashover1.mid'
-mid = MidiFile(filename)
-print('\nOriginal')
-track1 = mid.tracks[0]
-for msg in track1:
-	print(msg)
-	
-tpb = mid.ticks_per_beat
+#reconstructs a MIDI file from a list of beats
+def reconstructFromBeats(filename, sequence, ticks_per_beat, subdivisions = 2):
+	mid = MidiFile()
+	track = MidiTrack()
+	mid.tracks.append(track)
+	mid.ticks_per_beat = ticks_per_beat
+	divisions = int(ticks_per_beat / subdivisions)
 
-mid.save('output/realashover1.mid')
+	numticks = 0
+	curTone = 0
+	curVel = 0
 
-metaMessages = track1[0:4]
+	for beat in sequence:
+		tone, velocity = binToNote(beat)
+		# print(tone, velocity)
+		if tone != curTone:
+			if curTone != 0:
+				track.append(Message('note_off', note = curTone, velocity = 0, time = numticks))
+				numticks = 0
+			track.append(Message('note_on', note = tone, velocity = velocity, time = numticks))
+			numticks = 0
+			curTone = tone
+		numticks += divisions
 
-sequence = parseNotes(filename)
-# print(sequence.size())
-# for note in sequence:
-# 	print(note)
+	mid.save('output/' + filename)
+	return mid
 
-print('\nReconstructed:')
-mid = reconstructFromNotes('testashover1.mid', sequence, tpb)
-track2 = mid.tracks[0]
-for msg in track2:
-	print(msg)
+# # filename = '../Parker,_Charlie_-_Donna_Lee.midi'
+# filename = 'MIDI/melody/ashover1.mid'
+# mid = MidiFile(filename)
+# print('\nOriginal')
+# track1 = mid.tracks[0]
+# for msg in track1:
+# 	print(msg)
 
+# tpb = mid.ticks_per_beat
 
-# for i in range(len(track2)):
-# 	if track1[i] != track2[i]:
-# 		print(i)
-# 		print('\nTrack 1: ')
-# 		print(track1[i])
-# 		print('\nTrack 2: ')
-# 		print(track2[i])
-# 		print('\n')
+# # mid.save('output/realDonnaLee.mid')
+# mid.save('output/realAshover1.mid')
+
+# sequence = parseBeats(filename)
+# # for s in sequence:
+# # 	print(s)
+
+# print('\nReconstructed:')
+# mid = reconstructFromBeats('testAshover1.mid', sequence, tpb)
+# track2 = mid.tracks[0]
+# for msg in track2:
+# 	print(msg)
--- rnn_nottingham_dataset.py
+++ rnn_nottingham_dataset.py
@@ -1,37 +1,109 @@
+import torch
 import torch.nn as nn
-from parseMIDI import parseNotes, parseBeats
 
-class RNN(nn.module):
-    def __init__(self, input_size, hidden_size, output_size)
+from parseMIDI import parseBeats, reconstructFromBeats, getTpb
+
+class RNN(nn.Module):
+    def __init__(self, input_size, output_size, batch_size = 1):
         super(RNN, self).__init__()
 
-        self.hidden_size = hidden_size
-        self.lstm = nn.LSTMCell(input_size, hidden_size)
+        self.output_size = output_size
+        self.lstm = nn.LSTMCell(input_size, output_size)
+        self.hx = torch.randn(batch_size, output_size)
+        self.cx = torch.randn(batch_size, output_size)
 
-# def findFiles(path): 
-#     return glob.glob(path)
+    def forward(self, input):
+        self.hx, self.cx = self.lstm(input, (self.hx, self.cx))
+        return self.hx
 
-# print(findFiles('data/names/*.txt'))
+    def reset(self):
+        self.hx = torch.randn(batch_size, output_size)
+        self.cx = torch.randn(batch_size, output_size)
 
-# class RNN(nn.Module):
-#     def __init__(self, input_size, hidden_size, output_size):
-#         super(RNN, self).__init__()
+def createMidiFile(filename, output, tpb):
+    sequence = []
+    for o in output:
+        sequence += (o + 1).int().numpy().tolist()
+    return reconstructFromBeats(filename, sequence, tpb)
 
-#         self.hidden_size = hidden_size
+def constructTensorFromMidi(filename):
+    sequence = parseBeats(filename)
+    return (torch.tensor(sequence) - 1).view(len(sequence), 1, 14).float()
 
-#         self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-#         self.i2o = nn.Linear(input_size + hidden_size, output_size)
-#         self.softmax = nn.LogSoftmax(dim=1)
+rnn = RNN(14, 14)
 
-#     def forward(self, input, hidden):
-#         combined = torch.cat((input, hidden), 1)
-#         hidden = self.i2h(combined)
-#         output = self.i2o(combined)
-#         output = self.softmax(output)
-#         return output, hidden
+filename = 'MIDI/melody/ashover1.mid'
+# ashover1 = parseBeats('MIDI/melody/ashover1.mid')
+tpb = getTpb('MIDI/melody/ashover1.mid')
+# input = torch.tensor(ashover1).view(len(ashover1), 1, 14).float()
 
-#     def initHidden(self):
-#         return torch.zeros(1, self.hidden_size)
 
-# n_hidden = 128
-# rnn = RNN(n_letters, n_hidden, n_categories)
+# output = []
+# for i in range(len(input)):
+#     output += [rnn(input[i])]
+# sequence = constructTensorFromMidi(filename)
+# print(output[0][0])
+# print(sequence.long()[1][0])
+
+# mid = createMidiFile('test.mid',output, tpb)
+
+criterion = nn.MSELoss()
+learning_rate = 0.005
+
+import time
+import math
+
+n_iters = 100000
+print_every = 5000
+plot_every = 1000
+
+
+
+# Keep track of losses for plotting
+current_loss = 0
+all_losses = []
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+start = time.time()
+
+def train(filenames, numEpochs = 10):
+    output = []
+    for f in filenames:
+        sequence = constructTensorFromMidi(f)
+        compSeq = sequence
+        tpb = getTpb(f)
+        for i in range(numEpochs):
+            rnn.zero_grad()
+            output = []
+            for j in range(sequence.size()[0] - 1):
+                output += [rnn(sequence[j])]
+                loss = criterion(output[j][0], compSeq[j + 1][0])
+                loss.backward(retain_graph = True)
+                for p in rnn.parameters():
+                    p.data.add_(-learning_rate, p.grad.data)
+            print('Epoch: %d / %d, Elapsed time: %s' % (i + 1, numEpochs, timeSince(start)))
+            mid = createMidiFile('f_epoch_%d.mid' % i, output, tpb)   
+    mid = createMidiFile('f_final.mid',output, tpb)
+    return mid
+
+mid = train([filename])
+for msg in mid.tracks[0]:
+    print(msg)
+
+# rnn = nn.LSTMCell(10, 20)
+# input = torch.randn(6, 3, 10)
+# hx = torch.randn(3, 20)
+# cx = torch.randn(3, 20)
+# output = []
+# for i in range(6):
+#         hx, cx = rnn(input[i], (hx, cx))
+#         output.append(hx)
+
+# print(output)
